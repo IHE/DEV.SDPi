e58505ae3390da11b30ded569461be44f6027a9f
David Gregorczyk <david.gregorczyk@web.de>
2026-03-31 13:59:30 +0200

    build: #499 adapt batch scripts.

--- .ci/asciidoc-converter/build_document.bat
+++ .ci/asciidoc-converter/build_document.bat
@@ -1,1 +1,5 @@
-gradlew.bat run --args="--input-file ../../asciidoc/sdpi-standard.adoc --output-folder ../../sdpi-standard --backend html"
+mkdir ..\..\sdpi-documents
+mkdir ..\..\sdpi-documents\sdpi-standard
+mkdir ..\..\sdpi-documents\sdpi-supplement
+gradlew.bat run --args="--input-file ../../asciidoc/sdpi-standard.adoc --output-folder ../../sdpi-documents/sdpi-standard --backend html"
+gradlew.bat run --args="--input-file ../../asciidoc/sdpi-supplement.adoc --output-folder ../../sdpi-documents/sdpi-supplement --backend html"
--- .ci/asciidoc-converter/publish_assets.bat
+++ .ci/asciidoc-converter/publish_assets.bat
@@ -1,5 +1,11 @@
-xcopy ..\..\asciidoc\css\*.* ..\..\sdpi-standard\css /I /R /Y
-xcopy ..\..\asciidoc\fonts\*.* ..\..\sdpi-standard\fonts /I /R /Y
-xcopy ..\..\asciidoc\images\*.* ..\..\sdpi-standard\images /I /R /Y
-xcopy ..\..\asciidoc\js\*.* ..\..\sdpi-standard\js /I /R /Y
-xcopy images\*.* ..\..\sdpi-standard\images /I /R /Y
+xcopy ..\..\asciidoc\css\*.* ..\..\sdpi-documents\sdpi-standard\css /I /R /Y
+xcopy ..\..\asciidoc\fonts\*.* ..\..\sdpi-documents\sdpi-standard\fonts /I /R /Y
+xcopy ..\..\asciidoc\images\*.* ..\..\sdpi-documents\sdpi-standard\images /I /R /Y
+xcopy ..\..\asciidoc\js\*.* ..\..\sdpi-documents\sdpi-standard\js /I /R /Y
+xcopy images\*.* ..\..\sdpi-documents\sdpi-standard\images /I /R /Y
+
+xcopy ..\..\asciidoc\css\*.* ..\..\sdpi-documents\sdpi-supplement\css /I /R /Y
+xcopy ..\..\asciidoc\fonts\*.* ..\..\sdpi-documents\sdpi-supplement\fonts /I /R /Y
+xcopy ..\..\asciidoc\images\*.* ..\..\sdpi-documents\sdpi-supplement\images /I /R /Y
+xcopy ..\..\asciidoc\js\*.* ..\..\sdpi-documents\sdpi-supplement\js /I /R /Y
+xcopy images\*.* ..\..\sdpi-documents\sdpi-supplement\images /I /R /Y
